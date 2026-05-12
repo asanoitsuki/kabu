2dc30c1aecc39857db7141a70ef149bf36a0c954
あさのいつき <asaitsu0@gmail.com>
2026-05-12 23:40:41 +0900

    Update next.config.ts with new configuration options
    Added output and images configuration options to nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ output: 'export',      // これを追記
+ images: {
+   unoptimized: true,   // これを追記（画像表示エラーを防ぐ）
+ },
+ // もしパスの設定などで詰まったら、trailingSlashを追加することもあります
+ trailingSlash: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
